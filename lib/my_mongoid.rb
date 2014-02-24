30842e7ebb72e23200e41920239b8a5f09d10cf2
42thcoder <42thcoder@gmail.com>
2014-02-24 23:17:54 +0800

    Implement Configuration

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -2,6 +2,20 @@ require "my_mongoid/version"
 require "my_mongoid/document"
 require "my_mongoid/field"
 require "my_mongoid/errors"
+require "my_mongoid/config"
 
 module MyMongoid
+
+  def self.configuration
+    Configuration.instance
+  end
+
+  def self.configure
+    if block_given?
+      yield(configuration)
+    else
+      configuration
+    end
+  end
+
 end
---
+++ lib/my_mongoid/config.rb
@@ -0,0 +1,11 @@
+require 'singleton'
+
+module MyMongoid
+
+  class Configuration
+    include Singleton
+
+    attr_accessor :host, :database
+  end
+
+end
